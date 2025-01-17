55f26fce7fc3e6ac6bd902821b00e44afd40c963
Marianna Meaidi <mmeaidi@hjerteforeningen.dk>
2025-01-17 11:09:05 +0100

    opr 2023

--- data-raw/export_opr.R
+++ data-raw/export_opr.R
@@ -6,5 +6,5 @@ y <- data.table::fread("inst/extdata/export_opr_2023.txt", encoding = "UTF-8")
 y=y[year %in% c('2019', '2020','2021', '2022', '2023')]
 # usethis::use_data(export_opr, overwrite = TRUE)
 
-export_opr <- data.table::fread("inst/extdata/export_opr.txt", encoding = "UTF-8")
+export_opr <- rbind(x,y)
 usethis::use_data(export_opr, overwrite = TRUE)
