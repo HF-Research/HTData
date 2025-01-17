2b12b42f5ff3df37516846115b2180132503a7a9
Marianna Meaidi <mmeaidi@hjerteforeningen.dk>
2025-01-17 10:54:20 +0100

    updated med

--- data-raw/export_med.R
+++ data-raw/export_med.R
@@ -1,3 +1,10 @@
 ## code to prepare `export_med` dataset goes here
-export_med <- data.table::fread("inst/extdata/export_med.txt", encoding = "UTF-8")
+x <- data.table::fread("inst/extdata/export_med.txt", encoding = "UTF-8")
+# usethis::use_data(x, overwrite = TRUE)
+
+y <- data.table::fread("inst/extdata/export_med_2023.txt", encoding = "UTF-8")
+y=y[year %in% c('2019', '2020','2021', '2022', '2023')]
+# usethis::use_data(y, overwrite = TRUE)
+
+export_med <- rbind(x, y)
 usethis::use_data(export_med, overwrite = TRUE)
