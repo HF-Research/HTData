bb1b42b40c638e6659ff2207977ef1437d47a503
Marianna Meaidi <mmeaidi@hjerteforeningen.dk>
2025-01-27 15:48:26 +0100

    Updates removed 2024

--- data-raw/export_chd_age.R
+++ data-raw/export_chd_age.R
@@ -1,3 +1,4 @@
 ## code to prepare `export_opr` dataset goes here
 export_chd_age <- data.table::fread("inst/extdata/export_chd_age.txt", encoding = "UTF-8")
+export_chd_age <- export_chd_age[year != 2024]
 usethis::use_data(export_chd_age, overwrite = TRUE)
--- data-raw/export_chd_sex.R
+++ data-raw/export_chd_sex.R
@@ -1,3 +1,4 @@
 ## code to prepare `export_opr` dataset goes here
 export_chd_sex <- data.table::fread("inst/extdata/export_chd_sex.txt", encoding = "UTF-8")
+export_chd_sex <- export_chd_sex[year != 2024]
 usethis::use_data(export_chd_sex, overwrite = TRUE)
