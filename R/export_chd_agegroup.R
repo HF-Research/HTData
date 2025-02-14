79aafaafa30e267dcf0fd9c38432d04d6d33eb3c
Marianna Meaidi <mmeaidi@hjerteforeningen.dk>
2025-02-14 14:35:46 +0100

    added age_group as a R file

---
+++ R/export_chd_agegroup.R
@@ -0,0 +1,4 @@
+## code to prepare `export_age` dataset goes here
+export_chd_agegroup <- data.table::fread("inst/extdata/export_chd_agegroup.txt", encoding = "UTF-8")
+export_chd_agegroup <- export_chd_agegroup[year != 2024]
+usethis::use_data(export_chd_agegroup, overwrite = TRUE)
