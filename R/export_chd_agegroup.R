63c423744651bf28082da94e32e6777487290087
Marianna Meaidi <mmeaidi@hjerteforeningen.dk>
2025-02-16 19:40:36 +0100

    rkode ændring i fil

--- R/export_chd_agegroup.R
+++ R/export_chd_agegroup.R
@@ -1,4 +1,9 @@
-## code to prepare `export_age` dataset goes here
-export_chd_agegroup <- data.table::fread("inst/extdata/export_chd_agegroup.txt", encoding = "UTF-8")
-export_chd_agegroup <- export_chd_agegroup[year != 2024]
-usethis::use_data(export_chd_agegroup, overwrite = TRUE)
+#' CHD - sex data
+#'
+#' A dataset
+#'
+#'
+#' @format A data frame with 7772 rows and 2 variables:
+#'
+#' @source {HjerteTal data}
+"export_chd_agegroup"
