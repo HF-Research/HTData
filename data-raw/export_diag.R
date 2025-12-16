e39996063d639693be751787e23ebafda121d217
Cæcilie Stilling Denholt <caecilie.stilling.denholt.01@regionh.dk>
2025-12-16 15:14:33 +0100

    Update 12-2025 with data for 2024

--- data-raw/export_diag.R
+++ data-raw/export_diag.R
@@ -1,35 +1,35 @@
-## code to prepare `export_diag` dataset goes here
-data_files <-
-  list.files(path = "inst/extdata/",
-             pattern = "export_diag_2023",
-             full.names = TRUE)
-export_diag_2023 <-
-  lapply(data_files, data.table::fread, encoding = "UTF-8")
-export_diag_2023 <- data.table::rbindlist(export_diag_2023)
+# ## code to prepare `export_diag` dataset goes here
+# data_files <-
+#   list.files(path = "inst/extdata/",
+#              pattern = "export_diag_2024",
+#              full.names = TRUE)
+# export_diag_2023 <-
+#   lapply(data_files, data.table::fread, encoding = "UTF-8")
+# export_diag_2023 <- data.table::rbindlist(export_diag_2023)
 
 # usethis::use_data(export_diag_2023, overwrite = TRUE)
 
 ## code to prepare `export_diag` dataset goes here
 data_files <-
   list.files(path = "inst/extdata/",
-             pattern = "export_diag_new",
+             pattern = "export_diag_2024",
              full.names = TRUE)
 export_diag <-
   lapply(data_files, data.table::fread, encoding = "UTF-8")
 export_diag <- data.table::rbindlist(export_diag)
 
 # usethis::use_data(export_diag, overwrite = TRUE)
-
-
-x=export_diag[!(year %in% c('2022') & event %in% c("n.dead1", "n.dead30", "n.dead5", "n.dead.year"))]
-y <- export_diag_2023[year==2022 & event %in% c("n.dead1", "n.dead30", "n.dead5", "n.dead.year")]
-z <- rbind(x, y)
-
-y1 <- export_diag_2023[year %in% c('2019', '2020') & event == 'n.dead5']
-z1 <- rbind(z, y1)
-
-y2 <- export_diag_2023[year==2023]
-z2 <- rbind(z1, y2)
-export_diag <- z2[!(year %in% c('2023') & event %in% c("n.dead1", "n.dead30", "n.dead5", "n.dead.year"))]
+#
+#
+# x=export_diag[!(year %in% c('2022') & event %in% c("n.dead1", "n.dead30", "n.dead5", "n.dead.year"))]
+# y <- export_diag[year==2022 & event %in% c("n.dead1", "n.dead30", "n.dead5", "n.dead.year")]
+# z <- rbind(x, y)
+#
+# y1 <- export_diag[year %in% c('2019', '2020') & event == 'n.dead5']
+# z1 <- rbind(z, y1)
+#
+# y2 <- export_diag_2023[year==2023]
+# z2 <- rbind(z1, y2)
+# export_diag <- z2[!(year %in% c('2023') & event %in% c("n.dead1", "n.dead30", "n.dead5", "n.dead.year"))]
 
 usethis::use_data(export_diag, overwrite = TRUE)
--- data-raw/export_opr.R
+++ data-raw/export_opr.R
@@ -1,10 +1,10 @@
 ## code to prepare `export_opr` dataset goes here
-x <- data.table::fread("inst/extdata/export_opr.txt", encoding = "UTF-8")
-# usethis::use_data(export_opr, overwrite = TRUE)
-
-y <- data.table::fread("inst/extdata/export_opr_2023.txt", encoding = "UTF-8")
-y=y[year %in% c('2019', '2020','2021', '2022', '2023')]
-# usethis::use_data(export_opr, overwrite = TRUE)
-
-export_opr <- rbind(x,y)
+export_opr <- data.table::fread("inst/extdata/export_opr_2024.txt", encoding = "UTF-8")
 usethis::use_data(export_opr, overwrite = TRUE)
+#
+# y <- data.table::fread("inst/extdata/export_opr_2023.txt", encoding = "UTF-8")
+# y=y[year %in% c('2019', '2020','2021', '2022', '2023')]
+# # usethis::use_data(export_opr, overwrite = TRUE)
+#
+# export_opr <- rbind(x,y)
+# usethis::use_data(export_opr, overwrite = TRUE)
