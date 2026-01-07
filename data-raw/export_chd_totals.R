918bcb0e7be2e9ead38c00b5cc6690e4bf234206
Cæcilie Stilling Denholt <caecilie.stilling.denholt.01@regionh.dk>
2026-01-07 15:04:31 +0100

    Update CHD Jan 2026

--- data-raw/export_chd_totals.R
+++ data-raw/export_chd_totals.R
@@ -1,4 +1,4 @@
 ## code to prepare `export_opr` dataset goes here
 export_chd_totals <- data.table::fread("inst/extdata/export_chd_totals.txt", encoding = "UTF-8")
-export_chd_totals <- export_chd_totals[year != 2024]
+#export_chd_totals <- export_chd_totals[year != 2024]
 usethis::use_data(export_chd_totals, overwrite = TRUE)
